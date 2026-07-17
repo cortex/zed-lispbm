44751d3ccab72d5b106aa4075505eb8aa20d57fa
laxsjo <rasmus.soderhielm@gmail.com>
2026-07-17 10:45:32 +0200

    highlights.scm: Change error capture + fix brackets
    Prefer capturing the error nodes as `punctuation.special` in favor of
    `number`, as that produces a more distinctive color (in my theme at
    least, feal free to throw away this change if it looks bad in other
    themes).
    
    Also adds a capture of error, despite it not being in the official list
    of captures, since it's still possible to add a custom theme override
    targeting it.
    
    Also makes sure that bracket highlights only capture brackets which are
    part of complete bracket capture nodes. Previously the last bracket in
    the following snippet would be captured as `punctuation.bracket`, taking
    precedence over the error highlight. Now it's just highlighted using the
    error highlight.
    ```
    (nil))
    ```

--- languages/lispbm/highlights.scm
+++ languages/lispbm/highlights.scm
@@ -3,12 +3,13 @@
 (number) @number
 (directive) @preproc
 (string) @string
-["(" ")" "{" "}"] @punctuation.bracket
+(_ . "(" @punctuation.bracket ")" @punctuation.bracket .)
+(_ . "{" @punctuation.bracket "}" @punctuation.bracket .)
 (_ keyword:_ @keyword)
 (comment) @comment
 (function_definition name: (symbol) @function.name)
 (arglist (symbol) @variable.special )
-(ERROR) @number
+(ERROR) @number @punctuation.special @error
 (escape_sequence) @string.escape
 (invalid_escape_sequence) @warning
 (constant_symbol) @constant

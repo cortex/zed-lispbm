204a76c378a1a428ae2b99f3f6b4bd78d14c7b59
laxsjo <rasmus.soderhielm@gmail.com>
2026-07-17 10:58:25 +0200

    highlights.scm: Capture square brackets
    Makes sure that `[` and `]` are also captured as `punctuation.bracket`.

--- languages/lispbm/highlights.scm
+++ languages/lispbm/highlights.scm
@@ -5,6 +5,7 @@
 (string) @string
 (_ . "(" @punctuation.bracket ")" @punctuation.bracket .)
 (_ . "{" @punctuation.bracket "}" @punctuation.bracket .)
+(_ . "[" @punctuation.bracket "]" @punctuation.bracket .)
 (_ keyword:_ @keyword)
 (comment) @comment
 (function_definition name: (symbol) @function.name)
